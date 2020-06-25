170d5e187dbaa672bafc65c20fb94a518e7a6549
ShiokawaHayato <shiokawahayato@ShiokawaHayatonoMacBook-Pro.local>
2020-06-25 10:45:31 +0900

    add restar function

--- getStoreInfo.py
+++ getStoreInfo.py
@@ -1,10 +1,20 @@
-
 import requests
 import json
 from urllib import parse
 import pandas as pd
-
+import codecs
+import time
+import yaml
 #appid = "dj00aiZpPU9lblRWbnBmYU9SOSZzPWNvbnN1bWVyc2VjcmV0Jng9ZTI-"
+
+
+def checkKey(obj,key):
+    if key in obj:
+        return obj[key]
+    else:
+        return None
+    
+
 
 def getStoreInfos(gc,ac,start):
     payload = {
@@ -49,15 +59,20 @@ def getStoreInfos(gc,ac,start):
    
    
     url = "https://map.yahoo.co.jp/api/localsearch?" + parse.urlencode(payload) + "&gid={}".format(gid)
-   
+
+    print(url)
 
     response = requests.get(url)
+    #shop_num = response.count('id')
     suffix = response.text.split("(")[0] + "("
     json_f = response.text.replace(suffix,"").rstrip(")")
    
     elements = json.loads(json_f)
 
-    print(elements["Feature"][0])
+    #print(elements["Feature"][0])
+    if "Feature" not in elements.keys():
+        return None
+
     key_list = elements["Feature"][0].keys()
 
     header_list = [
@@ -112,10 +127,10 @@ def getStoreInfos(gc,ac,start):
 
         if "Station" in property.keys() and len(element["Property"]["Station"]) > 0:
             station = element["Property"]["Station"][0]
-            station_id_list.append(station["Id"])
-            station_name_list.append(station["Name"])
-            station_distance_list.append(station["Distance"])
-            station_time_list.append(station["Time"])
+            station_id_list.append(checkKey(station,"Id"))
+            station_name_list.append(checkKey(station,"Name"))
+            station_distance_list.append(checkKey(station,"Distance"))
+            station_time_list.append(checkKey(station,"Time"))
         else:
             station_id_list.append(None)
             station_name_list.append(None)
@@ -124,11 +139,11 @@ def getStoreInfos(gc,ac,start):
 
         if "Genre" in property.keys():
             genre = element["Property"]["Genre"][0]
-            genre_code_list.append(genre["Code"])
-            genre_name_list.append(genre["Name"])
+            genre_code_list.append(checkKey(genre,"Code"))
+            genre_name_list.append(checkKey(genre,"Name"))
         else:
-            genre_code_list.append(genre)
-            genre_name_list.append(genre)
+            genre_code_list.append(None)
+            genre_name_list.append(None)
 
         if "LocoReviewCount" in property.keys():
             rc = property["LocoReviewCount"]
@@ -172,8 +187,100 @@ def getStoreInfos(gc,ac,start):
        
     return df
 
+
 if __name__ == "__main__":
-    df =  getStoreInfos("01","25201","1")
-    print(df)
-    with open("sample_output.csv",mode="w",encoding="cp932",errors="ignore") as f:
-        df.to_csv(f)
+
+    with open("./conf.yaml") as f:
+        conf = yaml.safe_load(f)
+    print(conf)
+
+    #gc取得
+    gc_list = ["01", "02", "03", "04"]
+    
+    #ac取得
+    path = "./zenkoku.csv"
+    with codecs.open(path, "r", "Shift-JIS", "replace") as file:
+        j_cd = pd.read_table(file, delimiter=",")
+        # a_cd = cd_list['市区町村CD']
+        # ac_list = list(a_cd.unique().tolist())
+    #print(conf)
+
+    prefecture = int(conf["prefecture"])
+    df_1 = j_cd[j_cd['都道府県CD'] == prefecture]
+    df_2 = df_1['市区町村CD']
+    ac_list = list(df_2.unique().tolist())
+
+    df_list = []
+    tmp_list = []
+
+
+    count = 0
+
+
+    ac_restart_flg = True
+    for gc in gc_list:
+        #スキップ処理
+        if conf["gc"] != "":
+            if int(conf["gc"]) > int(gc):
+                print("gc : {} skip".format(gc))
+                continue
+
+        for ac in ac_list:
+            if len(str(ac)) < 5:
+                ac = "0" + str(ac)
+
+            #スキップ処理
+            if conf["ac"] != "":
+                if conf["ac"] != ac and ac_restart_flg == True:
+                    print("ac : {} skip".format(ac))
+                    continue
+                else:
+                    ac_restart_flg = False
+            else:
+                ac_restart_flg = False
+ 
+            if conf["start"] != "":
+                start = int(conf["start"])
+                print("restart : {}".format(start))
+            else:
+                start = 1
+
+            #データ取得
+            while(True):
+                print(start)
+                time.sleep(1)
+                df =  getStoreInfos(gc,ac,start)
+                if df is not None :
+                    df_list.append(df)
+                    tmp_list.append(df)
+                else:
+                    break
+                    
+
+                count += 1
+                if count % 10 == 0:
+                    tmp_result = pd.concat(tmp_list)
+                    print(tmp_result)
+                    with open("./tmp/{}_{}_{}.csv".format(gc,ac,count),mode="w",encoding=conf["encode"],errors="ignore") as f:
+                        tmp_result.to_csv(f)
+
+
+                    log_df = pd.DataFrame({
+                            "gc" :[gc],
+                            "ac" :[ac],
+                            "start" : [start]
+                            })
+                    with open("./log/log.csv".format(gc,ac,count),mode="w",encoding=conf["encode"],errors="ignore") as f:
+                        log_df.to_csv(f)
+
+                    tmp_list = []
+
+                if len(df) < 100:
+                    break
+                start += 100
+               
+    #result = pd.concat(df_list)
+    #print(result)
+    #with open("hokkaido.csv",mode="w",encoding="cp932",errors="ignore") as f:
+    #    result.to_csv(f)
+
---
+++ make_data.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+import yaml
+
+file_list = os.listdir(path = "./tmp/")
+with open("./conf.yaml") as f:
+    conf = yaml.safe_load(f)
+
+df_list = []
+for name in file_list:
+    df = pd.read_csv("./tmp/" + name,encoding="cp932")
+    df_list.append(df)
+
+result = pd.concat(df_list)
+
+dup = len(result[result["id"].duplicated()])
+length = len(result)
+print("データ総数 : {}".format(length))
+print("重複件数 : {}".format(dup))
+
+with open("output/output_{}.csv".format(conf["prefecture"]),mode="w",encoding="cp932",errors="ignore") as f:                                                                                                           
+        result.to_csv(f) 
+
